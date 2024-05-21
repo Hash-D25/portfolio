e504ee13d2925db032b839b3aa5221728337edcd
Hash-D25 <149588109+Hash-D25@users.noreply.github.com>
2024-05-21 22:20:17 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css'
+import '../styles/Components/header.css';
+import '../styles/Components/avatar.css';
+import '../styles/Components/about.css';
+import '../styles/utils.css';
